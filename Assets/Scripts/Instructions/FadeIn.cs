f71b860ca8033da9b357e79896466873751e380f
Jon Jenkins <jsj5909@hotmail.com>
2021-02-24 12:26:32 -0800

    Initial screen layout

---
+++ Assets/Scripts/Instructions/FadeIn.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeIn : MonoBehaviour
+{
+    [SerializeField] float startTime = 2f;
+    [SerializeField] float fadeTime = 3f;
+
+   
+    
+    float alpha = 0;
+
+    TextMeshProUGUI textMessage;
+
+    bool fadeStarted = false;
+    float elapsedTime = 0;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        textMessage = GetComponent<TextMeshProUGUI>();
+        if(textMessage == null)
+        {
+            textMessage = GetComponentInChildren<TextMeshProUGUI>();
+        }
+        textMessage.color = new Color(textMessage.color.r, textMessage.color.b, textMessage.color.g, alpha);
+       
+
+        StartCoroutine(WaitForFade());
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (fadeStarted)
+        {
+            if (elapsedTime < fadeTime)
+            {
+                alpha = Mathf.Lerp(0, 1, elapsedTime / fadeTime);
+                textMessage.color = new Color(textMessage.color.r, textMessage.color.b, textMessage.color.g, alpha);
+                Debug.Log(textMessage.color.a.ToString());
+                elapsedTime += Time.deltaTime;
+            }
+        }
+
+    }
+
+    IEnumerator WaitForFade()
+    {
+
+        yield return new WaitForSeconds(startTime);
+        fadeStarted = true;
+
+    }
+}
---
+++ Assets/Scripts/Instructions/FadeOut.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeOut : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI left;
+    [SerializeField] TextMeshProUGUI right;
+    [SerializeField] TextMeshProUGUI instructions;
+
+    [SerializeField] Button readyButton;
+
+    [SerializeField] float buttonActivationTime = 7;
+     
+
+    [SerializeField] float fadeTime = 2;
+
+    float elapsedTime = 0;
+    float buttonElapsedTime = 0;
+
+    bool fadeStarted = false;
+    bool readyButtonVisible = false;
+
+    float alpha = 1;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        readyButton.gameObject.SetActive(false);
+        readyButtonVisible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        buttonElapsedTime += Time.deltaTime;
+        
+        if(buttonElapsedTime > buttonActivationTime)
+        {
+            if (!readyButtonVisible)
+            {
+                readyButton.gameObject.SetActive(true);
+                readyButtonVisible = true;
+            }
+        }
+        
+        
+        if (fadeStarted)
+        {
+            if (elapsedTime < fadeTime)
+            {
+                alpha = Mathf.Lerp(1,0, elapsedTime / fadeTime);
+                left.color = new Color(left.color.r, left.color.g, left.color.b, alpha);
+                right.color = new Color(right.color.r, right.color.g, right.color.b, alpha);
+                instructions.color = new Color(instructions.color.r, instructions.color.g, instructions.color.b, alpha);
+                elapsedTime += Time.deltaTime;
+            }
+           
+        }
+
+    }
+
+
+    public void ReadyButtonPushed()
+    {
+        fadeStarted = true;
+
+       readyButton.gameObject.SetActive(false);
+        //readyButtonVisible = false;
+
+        
+    }
+
+}
