93a904789aa038b4e529f73e7046c68a7a32dc14
jsj5909 <jsj5909@hotmail.com>
2021-03-19 15:45:55 -0700

    Updated Vehicle and alignment

--- Assets/Scripts/TaxiController.cs
+++ Assets/Scripts/TaxiController.cs
@@ -7,250 +7,67 @@ using Random = UnityEngine.Random;
 public class TaxiController : MonoBehaviour
 {
     int curWP;
-    float rotSpeed = 2;//1.4f;
+    float rotSpeed = 1.4f;
     float speed = 15.5f;
     float accuracyWP = 5.0f;
 
+    [SerializeField] Transform[] wayPoints;
 
-    Vector3 currentWaypoint;
+    //[SerializeField] GameObject[] paths;
 
-    bool guidanceEnabled = true;
+    GameObject currentPath;
 
     LayerMask layerMask;
 
-
-    [SerializeField] Transform testWaypoint;
-
+    // Start is called before the first frame update
     void Start()
     {
-        layerMask = LayerMask.GetMask("Waypoints");
-        FindNextWaypoint();
+        currentPath = GameObject.FindGameObjectWithTag("StartingPath");
+        
+        wayPoints = currentPath.GetComponentsInChildren<Transform>();
+
+        Debug.Log("waypoints length: " + wayPoints.Length.ToString());
+
+        curWP = 1;
+
+        layerMask = LayerMask.GetMask("Terrain");
+      
     }
 
     // Update is called once per frame
-
-
-
     void Update()
     {
-        /*
-        if (Vector3.Distance(currentWaypoint, transform.position) < accuracyWP)
+        if (Vector3.Distance(wayPoints[curWP].transform.position, transform.position) < accuracyWP)
         {
-            FindNextWaypoint();
+            curWP++;
+            if (curWP >= wayPoints.Length)
+            {
+                curWP = 0;
+            }
         }
-        */
 
-        if (guidanceEnabled)
-        {
-            Vector3 direction = currentWaypoint - transform.position;
-            this.transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), rotSpeed * Time.deltaTime);
-        }
+        Vector3 direction = wayPoints[curWP].transform.position - transform.position;
+        this.transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), rotSpeed * Time.deltaTime);
         this.transform.Translate(0, 0, Time.deltaTime * speed);
 
+        RaycastHit hitInfo;
 
-     //   this.transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), rotSpeed * Time.deltaTime);
+        if(Physics.Raycast( transform.position, -transform.up, out hitInfo, Mathf.Infinity,layerMask))
+        {
+            //transform.up = hitInfo.normal;
+            transform.up -= (transform.up - hitInfo.normal) * 0.1f;
+
+        }
+
+      //  this.transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), rotSpeed * Time.deltaTime);
 
 
 
        // this.transform.Translate(0, 0, Time.deltaTime * speed);
 
-    }
-
-    void FindNextWaypoint()
-    {
-
-        RaycastHit hitInfo = new RaycastHit();
-
-        Debug.DrawRay(transform.position, transform.forward * 30,Color.red,5);
-
-
-       // (Camera.main.ScreenPointToRay(Input.mousePosition), out hitInfo, float.PositiveInfinity, layerMask);
-
-        if (Physics.Raycast(transform.position,transform.forward * 10, out hitInfo, Mathf.Infinity,layerMask))
-        {
-            currentWaypoint = hitInfo.collider.gameObject.transform.position;
-
-            Debug.Log("Next Waypoint: " + hitInfo.collider.gameObject.name);
-
-        }
-
-
 
     }
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.tag.Equals("Waypoint"))
-        {
-            guidanceEnabled = false;
-        }
-
-        else 
-        {/*
-            if(other.gameObject.tag.Equals("Turn"))
-            {
-               // Vector3 turnDirection = testWaypoint.transform.position - transform.position;
-               // this.transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(turnDirection), rotSpeed * Time.deltaTime);
-                transform.rotation = Quaternion.AngleAxis(-45, Vector3.up);
-                guidanceEnabled = false;
-            }*/
-        }
-    }
-/*
-    private void OnTriggerStay(Collider other)
-    {
-        if (other.gameObject.tag.Equals("Turn"))
-        {
-            Vector3 turnDirection = testWaypoint.transform.position - transform.position;
-            this.transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(turnDirection), rotSpeed * Time.deltaTime);
-        }
-    }
-*/
-
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.gameObject.tag.Equals("Waypoint")) ;
-        {
-            FindNextWaypoint();
-            guidanceEnabled = true;
-            Debug.Log("Exitting Collision");
-        }
-    }
 
 
-
-
-
-
-
-
-
-    /* [SerializeField] GameObject[] route;
-
-    int currentPath = 0;
-
-    public Transform taxi;
-
-   // public GameObject[] wayPoints;
-    int curWP = 0;
-    float rotSpeed = 1.4f;
-    float speed = 15.5f;
-    float accuracyWP = 5.0f;
-
-    Rigidbody rb;
-
-    Transform[] paths;
-    Transform[] waypoints;
-    Transform[] upcomingWaypoints;
-
-    bool approachingTurn = false;
-
-    public static Action ApproachingTurn;
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-
-        waypoints = route[currentPath].GetComponentsInChildren<Transform>();    //start at path 0
-
-        curWP = 1;
-
-        Debug.Log("Waypoint Length: " + waypoints.Length.ToString());
-        Debug.Log("Waypoint 1 location" + waypoints[1].transform.position.ToString());
-
-       // rb = GetComponent<Rigidbody>();
-
-    }
-
-    // Update is called once per frame
-
-
-
-    void Update()
-    {
-        if(waypoints[curWP].tag == "Turn" && !approachingTurn)
-        {
-            approachingTurn = true;
-
-         /*   if(ApproachingTurn != null)
-            {
-                ApproachingTurn();
-            }*/
-
-    /*       Debug.Log("------------TURN APPROACHING----------");
-
-           GenerateNextTurnData();
-       }
-
-       else if (Vector3.Distance(waypoints[curWP].transform.position, transform.position) < accuracyWP)
-       {
-           if (waypoints[curWP].tag == "Turn")
-           {
-               waypoints = upcomingWaypoints;
-               curWP = 1;
-               approachingTurn = false;
-           }
-           else
-           {
-               curWP++;
-               if (curWP >= waypoints.Length)
-               {
-                   curWP = 0;
-               }
-           }
-       }
-
-       Vector3 direction = waypoints[curWP].transform.position - transform.position;
-
-       this.transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), rotSpeed * Time.deltaTime);
-       this.transform.Translate(0, 0, Time.deltaTime * speed);
-
-       Debug.Log("Waypoint 1 location" + waypoints[curWP].gameObject.transform.position.ToString());
-   }
-
-
-   void GenerateNextTurnData()
-   {
-       GameObject nextPath;
-
-       int randomTurn = Random.Range(0, 2);
-
-
-
-       if (randomTurn == 0)
-       {
-           Debug.LogError("TurnLeft");
-
-           nextPath = waypoints[curWP].GetComponent<Turn>().left;
-
-
-       }
-       else
-       {
-           Debug.LogError("TurnRight");
-
-           nextPath = waypoints[curWP].GetComponent<Turn>().right;
-       }
-
-       upcomingWaypoints = nextPath.GetComponentsInChildren<Transform>();
-
-
-   }
-
-   //test code below not in use
-
-   // RaycastHit hit;
-   // if (Physics.Raycast(transform.position, -transform.up, out hit))
-   //  {
-   //     transform.up = hit.normal;
-   //  }
-
-   //    rb.freezeRotation = true;
-   //    this.transform.Translate(0, 0, Time.deltaTime * speed);
-   //  rb.velocity = transform.forward * speed * Time.deltaTime;
-
-
-   */
 }
-
---
+++ Assets/Scripts/Turn.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turn : MonoBehaviour
+{
+ 
+    
+    
+    
+    
+    /*  enum TurnDirection { NONE, LEFT, RIGHT };
+
+    [SerializeField] public GameObject left { get; }
+    [SerializeField] public GameObject right { get; }
+
+    bool turnPending = false;
+
+    TurnDirection nextTurn = TurnDirection.NONE;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       // TaxiController.ApproachingTurn += GenerateNextTurn;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(turnPending)
+        {
+            if(Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                if(nextTurn == TurnDirection.RIGHT)
+                {
+                    Debug.Log("Correct");
+                    turnPending = false;
+                }
+                else 
+                {
+                    Debug.Log("GAME OVER");
+                }
+            }
+
+            if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                if(nextTurn == TurnDirection.LEFT)
+                {
+                    Debug.Log("Correct");
+                    turnPending = false;
+                }
+                else
+                {
+                    Debug.Log("GAME OVER");
+                }
+            }
+
+
+        }
+    }
+
+    void GenerateNextTurn()
+    {
+        int randomTurn = Random.Range(0, 2);
+
+        if(randomTurn == 0)
+        {
+            Debug.LogError("TurnLeft");
+           // nextTurn = TurnDirection.left;
+        }
+        else
+        {
+            Debug.LogError("TurnRight");
+           // nextTurn = TurnDirection.right;
+        }
+
+        turnPending = true;
+    }*/
+}
