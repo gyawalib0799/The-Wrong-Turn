d24e39b27ea7f60750c279ab72eb10173c12219b
jsj5909 <jsj5909@hotmail.com>
2021-03-18 20:22:17 -0700

    turn code started

--- Assets/Scripts/TaxiController.cs
+++ Assets/Scripts/TaxiController.cs
@@ -5,24 +5,34 @@ using UnityEngine;
 public class TaxiController : MonoBehaviour
 {
     [SerializeField] GameObject[] route;
-    
+
+    int currentPath = 0;
     
     public Transform taxi;
 
-    public GameObject[] wayPoints;
-    int curWP;
+   // public GameObject[] wayPoints;
+    int curWP = 0;
     float rotSpeed = 1.4f;
     float speed = 15.5f;
     float accuracyWP = 5.0f;
 
     Rigidbody rb;
 
+    Transform[] paths;
+    Transform[] waypoints;
+
     // Start is called before the first frame update
     void Start()
     {
-        Transform[] waypoint = GetComponentsInChildren<Transform>();
+       
+        waypoints = route[currentPath].GetComponentsInChildren<Transform>();    //start at path 0
 
-        rb = GetComponent<Rigidbody>();
+        curWP = 1;
+
+        Debug.Log("Waypoint Length: " + waypoints.Length.ToString());
+        Debug.Log("Waypoint 1 location" + waypoints[1].transform.position.ToString());
+        
+       // rb = GetComponent<Rigidbody>();
 
     }
 
@@ -32,35 +42,45 @@ public class TaxiController : MonoBehaviour
     
     void Update()
     {
-        if (Vector3.Distance(wayPoints[curWP].transform.position, transform.position) < accuracyWP)
+        if(waypoints[curWP].tag == "Turn")
+        {
+
+            Debug.Log("------------TURN APPROACHING----------");
+        }
+        
+        else if (Vector3.Distance(waypoints[curWP].transform.position, transform.position) < accuracyWP)
         {
             curWP++;
-            if (curWP >= wayPoints.Length)
+            if (curWP >= waypoints.Length)
             {
                 curWP = 0;
             }
         }
 
-        Vector3 direction = wayPoints[curWP].transform.position - transform.position;
-        rb.freezeRotation = false;
+        Vector3 direction = waypoints[curWP].transform.position - transform.position;
+        
         this.transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), rotSpeed * Time.deltaTime);
+        this.transform.Translate(0, 0, Time.deltaTime * speed);
 
-        RaycastHit hit;
-        if (Physics.Raycast(transform.position, -transform.up, out hit))
-        {
-            transform.up = hit.normal;
-        }
-
-         rb.freezeRotation = true;
-          this.transform.Translate(0, 0, Time.deltaTime * speed);
-      //  rb.velocity = transform.forward * speed * Time.deltaTime;
+        Debug.Log("Waypoint 1 location" + waypoints[curWP].gameObject.transform.position.ToString());
     }
 
+   
+    
+    
+    //test code below not in use
 
-    private void OnCollisionEnter(Collision other)
-    {
-       // if(other.gameObject.tag == "Ground")
-         //   Debug.Log("Ground Normal" + other.GetContact()
-    }
+    // RaycastHit hit;
+    // if (Physics.Raycast(transform.position, -transform.up, out hit))
+    //  {
+    //     transform.up = hit.normal;
+    //  }
+
+    //    rb.freezeRotation = true;
+    //    this.transform.Translate(0, 0, Time.deltaTime * speed);
+    //  rb.velocity = transform.forward * speed * Time.deltaTime;
+
+
+
 }
 
