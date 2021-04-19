9b1f8e1c3003944d5116d3bd4224eab60078c43a
Bijay Gyawali <bgwbc@mail.umkc.edu>
2021-04-19 09:51:05 -0500

    add speedometer

---
+++ Assets/Speedometer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+public class Speedometer : MonoBehaviour
+{
+    public Rigidbody target;
+    public float maxSpeed = 22f;
+
+    public float minSpeedArrowAngle;
+    public float maxSpeedArrowAngle;
+
+    [Header("UI")]
+    public Text speedLabel;
+    public RectTransform arrow;
+    private float speed = 15.5f;
+    
+    public void Update(){
+        speed = target.velocity.magnitude * 3.6f;
+
+        if (speedLabel != null)
+            speedLabel.text = ((int)(speed*22)) + "km/hr";
+        if (arrow != null)
+            arrow.localEulerAngles = 
+                new Vector3(0,0, Mathf.Lerp(minSpeedArrowAngle, maxSpeedArrowAngle, speed/maxSpeed));
+    }
+
+}
