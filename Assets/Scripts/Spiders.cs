49f4032923af1aa440de02ce4c8aae8d407c1d61
jsj5909 <jsj5909@hotmail.com>
2021-05-07 14:06:00 -0700

    spider added to hood

---
+++ Assets/Scripts/Spiders.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spiders : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject landingPos;
+
+    [SerializeField]
+    private GameObject endPos;
+
+    [SerializeField]
+    private float speed = 5;
+
+    [SerializeField]
+    private float maxMove = 0.5f;
+
+    private bool hasLanded = false;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (hasLanded)
+        {
+            float step = maxMove * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, endPos.transform.position, step);
+        }
+        else
+        {
+            float step = maxMove * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, landingPos.transform.position, step);
+        }
+
+        if(Vector3.Distance(transform.position,landingPos.transform.position)<0.1)
+        {
+            hasLanded = true;
+        }
+
+    }
+}
