00f3dc3060d655c18f85db322bd5db3b9a983e74
jsj5909 <jsj5909@hotmail.com>
2021-05-09 23:57:28 -0700

    Added additional spiders and controller

---
+++ Assets/Scripts/SpiderController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiderController : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [SerializeField] GameObject[] spiders;
+
+
+    int previousSpider = -1;
+
+    void Start()
+    {
+        foreach(GameObject spider in spiders)
+        {
+            spider.SetActive(false);
+        }
+        
+        StartCoroutine(WaitForNextSpider());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+
+
+
+    }
+
+
+    IEnumerator WaitForNextSpider()
+    {
+        float waitTime = 0;
+        
+        while(true)
+        {
+             waitTime = UnityEngine.Random.Range(10, 20);
+            
+            
+            
+            yield return new WaitForSeconds(waitTime);
+            SpawnSpider();
+
+
+        }
+    }
+
+    private void SpawnSpider()
+    {
+        int spiderIndex = UnityEngine.Random.Range(0, spiders.Length);
+
+        while(spiderIndex == previousSpider)
+        {
+            spiderIndex = UnityEngine.Random.Range(0, spiders.Length);
+        }
+
+        spiders[spiderIndex].SetActive(true);
+        previousSpider = spiderIndex;
+    }
+}
--- Assets/Scripts/Spiders.cs
+++ Assets/Scripts/Spiders.cs
@@ -12,37 +12,167 @@ public class Spiders : MonoBehaviour
     private GameObject endPos;
 
     [SerializeField]
-    private float speed = 5;
+    private GameObject jumpDestination;
+
+    [SerializeField]
+    private GameObject originalPos;
 
     [SerializeField]
     private float maxMove = 0.5f;
 
+    [SerializeField]
     private bool hasLanded = false;
-    
+
+    [SerializeField]
+    private bool attackPlayer = false;
+
+    private bool hasAttacked = false;
+
+    private Animator animator;
+
+    private bool jumping = false;
+
+    private bool hasJumped = false;
+
+    private bool hasReachedDestination = false;
+
+    [SerializeField]
+    private float jumpSpeed = 2;
+
     // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
         
+    }
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+
+        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (hasLanded)
+        if (attackPlayer)
         {
-            float step = maxMove * Time.deltaTime;
-            transform.position = Vector3.MoveTowards(transform.position, endPos.transform.position, step);
+            if (hasLanded)
+            {
+                float step = maxMove * Time.deltaTime;
+              //  transform.position = Vector3.MoveTowards(transform.position, endPos.transform.position, step);
+
+              //  if ((Vector3.Distance(transform.position, endPos.transform.position) < 0.1f))
+              //  {
+                  //  HasReachedDestination();
+               // }
+                if (!hasReachedDestination)
+                {
+                    //animator.SetBool("ReachedDestination", true);
+                    // hasReachedDestination = true;
+                    transform.position = Vector3.MoveTowards(transform.position, endPos.transform.position, step);
+
+                      if ((Vector3.Distance(transform.position, endPos.transform.position) < 0.1f))
+                      {
+                      HasReachedDestination();
+                     }
+
+                }
+                else
+                {
+                    animator.SetBool("ReachedDestination", true);
+                    hasReachedDestination = true;
+
+                    if (hasAttacked)
+                    { 
+                    
+                      if(hasJumped)
+                        {
+                            float jumpStep = jumpSpeed * Time.deltaTime;
+                            transform.position = Vector3.MoveTowards(transform.position, jumpDestination.transform.position, jumpStep);
+
+                            if(Vector3.Distance(transform.position, jumpDestination.transform.position) < 0.1)
+                            {
+                                ResetSpider();
+                            }
+
+                        }
+                    }
+                    else
+                    {
+                        animator.SetBool("hasAttacked", true);
+                    }
+
+                }
+            
+            }
+            else
+            {
+                float step = maxMove * Time.deltaTime;
+                transform.position = Vector3.MoveTowards(transform.position, endPos.transform.position, step);
+            }
+
+            if (Vector3.Distance(transform.position, endPos.transform.position) < 0.1)
+            {
+                hasLanded = true;
+                hasReachedDestination = true;
+            }
+
         }
         else
         {
-            float step = maxMove * Time.deltaTime;
-            transform.position = Vector3.MoveTowards(transform.position, landingPos.transform.position, step);
+            if (hasLanded)
+            {
+                float step = maxMove * Time.deltaTime;
+                transform.position = Vector3.MoveTowards(transform.position, endPos.transform.position, step);
+
+                if (Vector3.Distance(transform.position, endPos.transform.position) < 0.1)
+                {
+                    ResetSpider();
+                }
+
+            }
+            else
+            {
+                float step = maxMove * Time.deltaTime;
+                transform.position = Vector3.MoveTowards(transform.position, landingPos.transform.position, step);
+            }
+
+            if (Vector3.Distance(transform.position, landingPos.transform.position) < 0.1)
+            {
+                hasLanded = true;
+            }
         }
 
-        if(Vector3.Distance(transform.position,landingPos.transform.position)<0.1)
-        {
-            hasLanded = true;
-        }
+    }
 
+    public void SpiderJump()
+    {
+        hasJumped = true;
+    }
+
+    public void HasReachedDestination()
+    {
+        hasReachedDestination = true;
+    }
+
+    public void SetHasAttacked()
+    {
+        hasAttacked = true;
+        
+       // jumping = true;
+    }
+
+    private void ResetSpider()
+    {
+        hasJumped = false;
+        hasReachedDestination = false;
+        hasLanded = false;
+        hasAttacked = false;
+        
+        transform.position = originalPos.transform.position;
+        gameObject.SetActive(false);
+        
     }
 }
